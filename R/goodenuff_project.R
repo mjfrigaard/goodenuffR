1fff3627ced01a7c30bdb122f1fcbb918d80f21d
“Martin <mjfrigaard@gmail.com>
2022-09-12 22:10:51 -0700

    new folder path arguments

--- R/goodenuff_project.R
+++ R/goodenuff_project.R
@@ -9,19 +9,19 @@
 #' @description Create and open a new 'good enough' project.
 #' @param project_name name of your project
 #' @param parent_folder the parent folder of your project
-#'
 goodenuff_project <- function(project_name, folder_path) {
   # check for folder_path
   fs::dir_create(path = folder_path)
   folder_path <- base::suppressWarnings(base::normalizePath(folder_path))
-  # clean string for project name
+  # # replace white-space
   clean_project_name <- stringr::str_replace_all(project_name, "\\s+", "_")
-  # create path to project
+  # # create path to project
   project_path <- base::paste0(folder_path, "/", clean_project_name, "/")
-  # # initialize project
+  # # # initialize project
   rstudioapi::initializeProject(path = project_path)
-  # # full project path (for opening in new session)
+  # # # full project path (for opening in new session)
   rproj_full_project_path <- base::paste0(project_path, clean_project_name, ".Rproj")
-  # open new session
+  # # open new session
   rstudioapi::openProject(path = rproj_full_project_path, newSession = TRUE)
+  return(folder_path)
 }
